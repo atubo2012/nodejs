f47b1d519077e3a75b784479cd560dbe5f38caca
sean <aaa@aa.com>
2017-09-29 14:33:51 +0800

    1、新增getToday(),getNow()函数 2、[BUG]修复中原核定折扣率函数中错将总价折扣设置为面积折扣的bug。 3、[BUG]优化日志函数。

--- cheerio/utils.js
+++ cheerio/utils.js
@@ -4,6 +4,24 @@
  * 将日期格式化
  */
 exports.formatDate = function(date, style) {
+    return fmd(date,style);
+};
+
+/**
+ * 取得今天的日期
+ */
+exports.getToday = function() {
+    return fmd(new Date(),'yyyyMMdd');
+};
+
+/**
+ * 取得现在的时间
+ */
+exports.getNow = function() {
+    return fmd(new Date(),'hhmmss');
+};
+
+function fmd(date, style) {
     let y = date.getFullYear();
     let M = "0" + (date.getMonth() + 1);
     M = M.substring(M.length - 2);
@@ -16,13 +34,14 @@ exports.formatDate = function(date, style) {
     let s = "0" + date.getSeconds();
     s = s.substring(s.length - 2);
     return style.replace('yyyy', y).replace('MM', M).replace('dd', d).replace('hh', h).replace('mm', m).replace('ss', s);
-};
+}
 
 /**
  * 根据相关因素计算【核定折扣】，(核定折扣*挂牌均价/小区均价)=笋度。
- * @param size 面积
- * @param floor 楼层
- * @param tprice 总价
+ * @param size 面积 4位数值型
+ * @param floor 楼层 2位数值型
+ * @param tprice 总价（单位万） 数值型
+ * @param bdyear 建造年份 4位数值型
  */
 exports.getCfmDisct = function(size,floor,tprice,bdyear){
 
@@ -36,9 +55,9 @@ exports.getCfmDisct = function(size,floor,tprice,bdyear){
 
     let tpriceDisct = 1;
     if(tprice>1000 && tprice<=2000){
-        sizeDisct = 0.9;
+        tpriceDisct = 0.9;
     }else if(tprice>2000){
-        sizeDisct = 0.8;
+        tpriceDisct = 0.8;
     }
     //console.log(tprice+'万:'+tpriceDisct*10+'折');
 
@@ -86,11 +105,62 @@ exports.getCfmDisct = function(size,floor,tprice,bdyear){
  * 参数值一般填写：开始XXXX、结束XXXXX
  */
 exports.showLog = function(msg) {
-    let mem = process.memoryUsage();
-    let format = function(bytes) {
-        return (bytes/1024/1024).toFixed(2)+'MB';
-    };
-    console.log('rss ' + format(mem.rss)+' ht '+format(mem.heapTotal) + ' hu ' + format(mem.heapUsed) + ' :'+msg);
-
+    log(msg);
 };
+
+/**
+ * 输出日志
+ * @param msg
+ */
+function log(msg) {
+        let mem = process.memoryUsage();
+        let format = function(bytes) {
+            return (bytes/1024/1024).toFixed(2);
+        };
+        let now = fmd(new Date(),'hhmmss');
+        console.log('['+now+']'+'[Mem' + format(mem.rss)+'/'+format(mem.heapTotal) + '/' + format(mem.heapUsed) + ']:['+msg+']');
+}
+
+
+/**
+ * 导出形如excel文件
+ * @param dataArray 数组类型的数据
+ * @param path 以/结尾的路径名
+ * @param filename 文件名
+ */
+exports.exp2xls = function(dataArray,path,filename) {
+
+    let xlsx = require('node-xlsx');
+    let fs = require('fs');
+
+    log('开始生成EXCEL');
+    let file = xlsx.build([{
+        name: 'sheet1',
+        data: dataArray
+    }]);   //构建xlsx对象
+
+    fs.writeFileSync(path+filename+'.xlsx', file, 'binary'); // 写入
+    log('完成生成EXCEL');
+};
+
+
+/**
+ * 检查一个字符串是否为数字
+ * @param str
+ * @returns {boolean}
+ */
+exports.isNumber = function (str) {
+    let a = parseFloat(str);
+
+    if(isNaN(a)){
+        log(str +' is not number '+a);
+        return false;
+    }else{
+        log(str +' is  number '+a);
+        return true;
+    }
+};
+
+
+
 
